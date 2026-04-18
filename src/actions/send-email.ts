77ccbc69c0a66ac4401e2ac8ae04bc86ddbef74b
Harihara Subramanian <hariharasubramanian18@gmail.com>
2026-04-18 11:27:06 +0530

    portfolio

---
+++ src/actions/send-email.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import { Resend } from 'resend';
+
+import { env } from '@/env.mjs';
+import { TFormSchema } from '@/lib/form-schema';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmailAction = async ({ email, message }: TFormSchema) => {
+  try {
+    await resend.emails.send({
+      from: 'Portfolio <onboarding@resend.dev>',
+      to: 'hariharasubramanian18@gmail.com',
+      subject: 'Message from contact form',
+      replyTo: email,
+      text: `email: ${email} \nmessage: ${message}`,
+    });
+
+    return {
+      data: 'Email sent successfully!',
+    };
+  } catch {
+    return {
+      error: `Something went wrong!`,
+    };
+  }
+};
---
+++ src/lib/site-config.ts
@@ -0,0 +1,33 @@
+import { env } from '@/env.mjs';
+
+export const siteConfig = {
+  title: 'Harihara Subramanian | Scientific Writer',
+  description:
+    "Hi! I'm Harihara. I'm a scientific writer, studying biotechnology. I enjoy research and excel in translating technical content into clear, structured and user-oriented documentation.",
+  keywords: [
+  'Harihara Subramanian',
+  'Technical Writer',
+  'Scientific Writer',
+  'Researcher',
+  'Biotechnology',
+  'Medical Writing',
+  'Scientific Communication',
+  'Technical Documentation',
+  'Health Content Writing',
+  'Evidence-Based Writing',
+  'Science Communication',
+  'Molecular Biology Research',
+  'Cell Biology',
+  'Cancer Research',
+  'Network Pharmacology',
+  'Molecular Docking',
+  'Bioinformatics',
+  'Research to Communication',
+  'Translating Scientific Research',
+  'Scientific Content Development',
+  'Data Interpretation',
+  'Literature Review and Analysis',
+  ],
+  url: env.SITE_URL || 'https://example.com',
+  googleSiteVerificationId: env.GOOGLE_SITE_VERIFICATION_ID || '',
+};
